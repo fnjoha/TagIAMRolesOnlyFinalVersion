39a4bf557a0811949bccd169d8c04ffb6c0d235f
fnjoha <104035961+fnjoha@users.noreply.github.com>
2024-10-03 15:22:32 -0500

    Add files via upload

---
+++ TagIAMRolesOnlyFinalVersion2.0.ps1
@@ -0,0 +1,14 @@
+﻿# Provide the path to your CSV file
+$CSV_FILE = "C:\Users\fnjoh\AWS CSV files And Scripts\IAMNottagged2024Folder\IAMRoleUntaggedSamplequotesMode.txt"
+
+# Read the CSV file and add tags to IAM roles
+Get-Content $CSV_FILE | ForEach-Object {
+    # Split CSV columns into variables
+    $roleName, $tags = $_ -split ','
+
+       # Print the tags for debugging
+    Write-Host "Role: $roleName, Tags: $tags"
+
+    # Add tags to IAM role
+    aws iam tag-role --role-name $roleName  --tags Key=zotec:team,Value="Information Security" --profile Prod
+}
